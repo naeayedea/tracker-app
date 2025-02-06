fd19ecf764937d5b916c8b6144e8f29793103db8
naeayedea <steven.muirhead1999@gmail.com>
2025-02-06 23:06:08 +0000

    fixed not being able to delete your last tracker

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -3,18 +3,22 @@ import { useState, useEffect } from 'react';
 function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T | ((val: T) => T)) => void] {
     // Initialize state with initialValue
     const [storedValue, setStoredValue] = useState<T>(initialValue);
+    const [loaded, setLoaded] = useState<boolean>(false);
 
     useEffect(() => {
         const item = window.localStorage.getItem(key);
         setStoredValue(item != undefined ? JSON.parse(item) as T : initialValue);
+        setLoaded(true);
+
+        // only do this hook on load
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     useEffect(() => {
-        if (JSON.stringify(storedValue) !== JSON.stringify(initialValue)) {
+        if (loaded) {
             window.localStorage.setItem(key, JSON.stringify(storedValue));
-        }
-
-    }, [storedValue]);
+        } 
+    }, [key, loaded, storedValue]);
 
     return [storedValue, setStoredValue];
 }
